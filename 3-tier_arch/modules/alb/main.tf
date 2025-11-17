145b4afe0b7ddb40f9e2bc32ede78596287690b8
nikhilganorkar <nikhilganorkar0@gmail.com>
2025-11-17 19:16:51 +0530

    update commit

--- 3-tier_arch/modules/alb/main.tf
+++ 3-tier_arch/modules/alb/main.tf
@@ -4,30 +4,30 @@ resource "aws_security_group" "alb" {
   vpc_id      = var.vpc_id
   
   ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
+    from_port = 80
+    to_port   = 80
+    protocol  = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
   
   ingress {
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
+    from_port = 443
+    to_port   = 443
+    protocol  = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
   
   egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
+    from_port = 0
+    to_port   = 0
+    protocol  = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
   
   tags = {
-    Name        = "${var.project_name}-${var.environment}-alb-sg"
+    Name = "${var.project_name}-${var.environment}-alb-sg"
     Environment = var.environment
-    Project     = var.project_name
+    Project = var.project_name
   }
 }
 
